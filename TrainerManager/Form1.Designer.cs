fc25b57c89fe697fb37052a34defacfdcb055295
Matthew Shoemaker <matthew.shoemaker@gmail.com>
2016-11-04 17:48:00 -0500

    Added timers, icons, leds

--- TrainerManager/Form1.Designer.cs
+++ TrainerManager/Form1.Designer.cs
@@ -29,11 +29,103 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tmrSendManagerHeartbeat = new System.Windows.Forms.Timer(this.components);
+            this.tmrReceiveManagerHeartbeat = new System.Windows.Forms.Timer(this.components);
+            this.tmrReceiveClientHeartbeat = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmrSendManagerHeartbeat
+            // 
+            this.tmrSendManagerHeartbeat.Enabled = true;
+            this.tmrSendManagerHeartbeat.Interval = 5000;
+            this.tmrSendManagerHeartbeat.Tick += new System.EventHandler(this.tmrSendManagerHeartbeat_Tick);
+            // 
+            // tmrReceiveManagerHeartbeat
+            // 
+            this.tmrReceiveManagerHeartbeat.Tick += new System.EventHandler(this.tmrReceiveManagerHeartbeat_Tick);
+            // 
+            // tmrReceiveClientHeartbeat
+            // 
+            this.tmrReceiveClientHeartbeat.Tick += new System.EventHandler(this.tmrReceiveClientHeartbeat_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "T2 F15C Trainer Manager";
+            this.notifyIcon1.BalloonTipTitle = "TrainerManager";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "TrainerManager";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(12, 97);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(474, 173);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "red.bmp");
+            this.imageList1.Images.SetKeyName(1, "green.bmp");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(498, 424);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TrainerManager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer tmrSendManagerHeartbeat;
+        private System.Windows.Forms.Timer tmrReceiveManagerHeartbeat;
+        private System.Windows.Forms.Timer tmrReceiveClientHeartbeat;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
--- TrainerManager/Form1.cs
+++ TrainerManager/Form1.cs
@@ -7,14 +7,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Xml;
 
 namespace TrainerManager
 {
     public partial class Form1 : Form
     {
+        string machineListFile = ConfigurationManager.AppSettings["machineList"];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void outputFile()
+        {
+
+        }
+
+        private void readFile()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(machineListFile);
+
+            foreach(XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                foreach (XmlNode trainerNode in node)
+                {
+                    string ip = trainerNode.Attributes["ipAddress"]?.InnerText;
+                    string status = trainerNode.Attributes["status"]?.InnerText;
+                }
+            }
+        }
+
+        private void sendManagerHeartbeat()
+        {
+
+        }
+
+        private void receiveManagerHeartbeat()
+        {
+
+        }
+
+        private void receiveClientHeartbeat()
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            listView1.Columns.Add("Trainer #", 100);
+            listView1.Columns.Add("IP Addres", 100);
+            listView1.Columns.Add("Status", 100);
+
+            // read machine list from file, local machines only?  Entire map?
+            readFile();
+
+            // display table of machines read from the file, marked as RED until they respond
+
+            // start timer that will listen for client heartbeats
+
+            // start timer that will listen for manager heartbeats
+
+            // start timer that will send manager heartbeats
+        }
+
+        private void tmrReceiveClientHeartbeat_Tick(object sender, EventArgs e)
+        {
+            // check network for client heartbeats
+
+        }
+
+        private void tmrReceiveManagerHeartbeat_Tick(object sender, EventArgs e)
+        {
+            // check network for manager heartbeats
+
+        }
+
+        private void tmrSendManagerHeartbeat_Tick(object sender, EventArgs e)
+        {
+            // send manager heartbeat
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if(FormWindowState.Minimized == this.WindowState)
+            {
+ //               this.notifyIcon1.Visible = true;
+ //               this.notifyIcon1.ShowBalloonTip(500);
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+//            this.notifyIcon1.Visible = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(e.CloseReason == System.Windows.Forms.CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
